55ac3993ec00d2c17c4026b7efba25a818633d6e
cjdigitalhumanity <dighumanitymd@gmail.com>
2023-10-04 06:38:01 -0400

    Update WaterwatchApplication.java

--- src/main/java/com/ebisys/waterwatch/WaterwatchApplication.java
+++ src/main/java/com/ebisys/waterwatch/WaterwatchApplication.java
@@ -8,7 +8,7 @@ public class WaterwatchApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(WaterwatchApplication.class, args);
-		System.out.println("hello Spring");
+		System.out.println("hello Spring boot");
 		
 	}
 	
